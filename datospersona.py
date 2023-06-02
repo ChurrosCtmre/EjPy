c6a754ed6b38e7214cbcd3efad2f4d171310bba6
ChurrosCtmre <@edugonzalezsilva72@gmail.com>
2023-06-02 03:33:43 -0400

    Ejercicio en el que el usuario ingresa sus datos y luego muestra un enunciado con estos mismos

---
+++ datospersona.py
@@ -0,0 +1,6 @@
+nombre=input("Escriba su nombre por favor:\n")
+edad=input("Ingrese su edad por favor:\n")
+direccion=input("Indique su dirección por favor:\n")
+telefono=input("Escriba su número telefónico por favor:\n")
+datospersona=[nombre, edad, direccion, telefono]
+print (datospersona[0], "tiene", datospersona[1], "años, vive en", datospersona[2], "y su número telefónico es", datospersona[3])
