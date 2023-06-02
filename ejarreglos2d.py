910509683d7c0e6fc1a7181371d5539af0eadb11
ChurrosCtmre <@edugonzalezsilva72@gmail.com>
2023-06-02 12:50:14 -0400

    Ejercicio ppt arreglos bidimensionales

---
+++ ejarreglos2d.py
@@ -0,0 +1,6 @@
+import numpy as np
+import random as rd
+matriz=np.array([[rd.randint(0,100), rd.randint(0,100), rd.randint(0,100)],
+                 [rd.randint(0,100), rd.randint(0,100), rd.randint(0,100)],
+                 [rd.randint(0,100), rd.randint(0,100), rd.randint(0,100)]])
+print(matriz)
